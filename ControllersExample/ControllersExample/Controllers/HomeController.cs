3a1bc4dd6dcc92005bf95bd7c8b595d0534443a0
Helge Stegemoen <helge@stegemoen.com>
2025-08-19 22:47:14 +0200

    Added home, about and contact-us routes.

--- ControllersExample/ControllersExample/Controllers/HomeController.cs
+++ ControllersExample/ControllersExample/Controllers/HomeController.cs
@@ -4,11 +4,27 @@ namespace ControllersExample.Controllers;
 
     public class HomeController
     {
-        [Route("sayhello")] // Press F12 when cursor is over Route, to go to decleration
-        public string method1()
+        [Route("home")] // Press F12 when cursor is over Route, to go to decleration
+        [Route("/")]       
+        public string Index()
         {
             return "Hello from method1";
         }
+
+        [Route("about")]
+        public string About()
+        {
+            return "Hello from About";
+        }
+        
+        [Route("contact-us")]
+        [Route("contact")]
+        public string Contact()
+        {
+            return "Hello from Contact";
+        }
+        
+        
     }
 
 
